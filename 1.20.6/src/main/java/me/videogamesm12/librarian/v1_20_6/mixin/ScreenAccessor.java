f0b74eae693b2dddd1517b1216f44789aeff970e
Video <videogamesm12@gmail.com>
2025-01-11 15:59:55 -0700

    Adds support for 1.20.6

---
+++ 1.20.6/src/main/java/me/videogamesm12/librarian/v1_20_6/mixin/ScreenAccessor.java
@@ -0,0 +1,15 @@
+package me.videogamesm12.librarian.v1_20_6.mixin;
+
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.List;
+
+@Mixin(Screen.class)
+public interface ScreenAccessor
+{
+	@Accessor
+	List<Drawable> getDrawables();
+}
