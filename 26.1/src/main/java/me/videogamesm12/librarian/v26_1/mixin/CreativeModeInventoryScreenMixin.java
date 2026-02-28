f4963e8eaa5f91ae8b4d4fbb482f6cfcac6f6005
Video <videogamesm12@gmail.com>
2026-02-28 04:58:46 -0700

    We no longer need getSelectedTab in 26.1

--- 26.1/src/main/java/me/videogamesm12/librarian/v26_1/mixin/CreativeModeInventoryScreenMixin.java
+++ 26.1/src/main/java/me/videogamesm12/librarian/v26_1/mixin/CreativeModeInventoryScreenMixin.java
@@ -54,7 +54,6 @@ import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -68,6 +67,8 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 {
 	@Shadow protected abstract void selectTab(CreativeModeTab group);
 
+	@Shadow
+	private static CreativeModeTab selectedTab;
 	@Unique
 	private IMechanicFactory mechanic;
 
@@ -122,19 +123,29 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 			@Override
 			public boolean isVisible()
 			{
-				return tabIsHotbar(getSelectedTab());
+				return tabIsHotbar(selectedTab);
 			}
 
 			@Override
 			public boolean isActive()
 			{
-				return tabIsHotbar(getSelectedTab()) && isFocused();
+				return tabIsHotbar(selectedTab) && isFocused();
 			}
 
 			@Override
-			public void onClick(MouseButtonEvent event, boolean bl)
+			public boolean mouseClicked(MouseButtonEvent event, boolean bl)
 			{
-				setFocused(true);
+				Librarian.getLogger().fatal("Debug - mouse clicked event");
+
+				if (!isFocused())
+				{
+					Librarian.getLogger().fatal("Not focused");
+					setFocused(true);
+					return true;
+				}
+
+				Librarian.getLogger().fatal("Focused");
+				return super.mouseClicked(event, bl);
 			}
 		};
 
@@ -147,7 +158,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 		// Update the label if we are set to use the HOTBAR ItemGroup type
 		// This primarily aims to emulate vanilla behavior and avoid lagspikes when opening the creative menu whilst
 		// 	the current page isn't loaded
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			renameHotbarField.setMessage(mechanic.createText(Librarian.getInstance().getCurrentPage().librarian$getMetadata()
 					.map(HotbarPageMetadata::getName).orElse(net.kyori.adventure.text.Component.translatable("librarian.saved_hotbars.tab",
@@ -156,8 +167,8 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 
 		// Even though we override the isVisible and isActive methods, internally ClickableWidget still uses the
 		// 	variable themselves to determine other characteristics, so we still need to set them
-		renameHotbarField.active = tabIsHotbar(getSelectedTab());
-		renameHotbarField.visible = tabIsHotbar(getSelectedTab());
+		renameHotbarField.active = tabIsHotbar(selectedTab);
+		renameHotbarField.visible = tabIsHotbar(selectedTab);
 
 		// Initialize buttons
 		nextButton = mechanic.createButton(x + 12, y,12, 12, net.kyori.adventure.text.Component.text("→"),
@@ -169,10 +180,10 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 				net.kyori.adventure.text.Component.text("Previous page"), () -> Librarian.getInstance().previousPage());
 
 		// Marks visibility and usability of the buttons
-		nextButton.visible = tabIsHotbar(getSelectedTab());
-		backupButton.visible = tabIsHotbar(getSelectedTab());
+		nextButton.visible = tabIsHotbar(selectedTab);
+		backupButton.visible = tabIsHotbar(selectedTab);
 		backupButton.active = Librarian.getInstance().getCurrentPage().exists();
-		previousButton.visible = tabIsHotbar(getSelectedTab());
+		previousButton.visible = tabIsHotbar(selectedTab);
 
 		// Adds the "rename hotbar" text field
 		addRenderableWidget(renameHotbarField);
@@ -236,7 +247,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	@Inject(method = "renderLabels", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/CreativeModeTab;showTitle()Z", shift = At.Shift.AFTER), cancellable = true)
 	public void cancelForegroundTextRendering(GuiGraphics context, int mouseX, int mouseY, CallbackInfo ci)
 	{
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			ci.cancel();
 		}
@@ -245,7 +256,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	@Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
 	public void workaroundTypingInRenameField(KeyEvent input, CallbackInfoReturnable<Boolean> cir)
 	{
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			// Special keys
 			renameHotbarField.keyPressed(input);
@@ -297,7 +308,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	public void handleNavigationKeys(KeyEvent input, CallbackInfoReturnable<Boolean> cir)
 	{
 		// Librarian-specific keybinds
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			// Fabric API keybinds
 			final FabricAPIAddon fabric = Librarian.getInstance().getAddon(FabricAPIAddon.class);
@@ -388,7 +399,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	@Inject(method = "charTyped", at = @At("HEAD"), cancellable = true)
 	private void injectCharTyped(CharacterEvent input, CallbackInfoReturnable<Boolean> cir)
 	{
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			if (renameHotbarField.charTyped(input))
 			{
@@ -455,7 +466,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	public void onNavigation(NavigationEvent event)
 	{
 		// Refresh!
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			selectTab(BuiltInRegistries.CREATIVE_MODE_TAB.getValue(CreativeModeTabs.HOTBAR));
 		}
@@ -465,7 +476,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	@Unique
 	public void onReload(ReloadPageEvent event)
 	{
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			selectTab(BuiltInRegistries.CREATIVE_MODE_TAB.getValue(CreativeModeTabs.HOTBAR));
 		}
@@ -475,7 +486,7 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	@Unique
 	public void onCacheClear(CacheClearEvent event)
 	{
-		if (tabIsHotbar(getSelectedTab()))
+		if (tabIsHotbar(selectedTab))
 		{
 			selectTab(BuiltInRegistries.CREATIVE_MODE_TAB.getValue(CreativeModeTabs.HOTBAR));
 		}
@@ -486,7 +497,4 @@ public abstract class CreativeModeInventoryScreenMixin extends Screen
 	{
 		return group.getType() == CreativeModeTab.Type.HOTBAR;
 	}
-
-	@Accessor
-	public abstract CreativeModeTab getSelectedTab();
 }
